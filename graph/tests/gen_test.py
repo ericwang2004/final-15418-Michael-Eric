d6e2d5ac8f079becd8409f01d91e4c9507d79263
ericwang2004 <ericwang2004@gmail.com>
2025-11-19 13:23:53 -0500

    randomized order of edges

--- graph/tests/gen_test.py
+++ graph/tests/gen_test.py
@@ -1,9 +1,15 @@
+import random
+
 n_verts = 100
 n_edges = n_verts * (n_verts - 1) // 2
 
-with open("k{}.txt".format(n_verts), "w") as f:
+with open(f"k{n_verts}.txt", "w") as f:
     f.write(f"{n_verts} {n_edges}\n")
+    pairs = []
     for i in range(n_verts):
         for j in range(n_verts):
             if i != j:
-                f.write(f"{i} {j}\n")
+                pairs.append((i, j))
+    random.shuffle(pairs)
+    for i, j in pairs:
+        f.write(f"{i} {j}\n")
